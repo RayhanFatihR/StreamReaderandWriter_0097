7698bd344268e9419e490193ddd4906d3d74ed65
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-06-10 15:17:40 +0700

    Tahap 11

---
+++ manualExeption.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try 
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; // Melemparkan sebuah interger maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch(int a)
+    {
+        //blok 
+    }
+}
