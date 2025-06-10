c66e043a180fd12bb0236955f7b0830f4f494677
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-06-10 15:30:34 +0700

    Tahap 19

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -26,4 +26,8 @@ int main()
         //menulis dan memasukan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+    //membuka file dalam mode membaca
+    ifstream infile;
 }
